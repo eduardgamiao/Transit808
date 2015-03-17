b295a10670f4bb2932fbfd91cd1438decbd1e6d3
Eduard Gamiao <edgamiao@gmail.com>
2015-03-16 23:58:59 -1000

    Trip front-end.

--- app/src/main/java/ics466uhm/transit808/Trips.java
+++ app/src/main/java/ics466uhm/transit808/Trips.java
@@ -1,26 +1,23 @@
 package ics466uhm.transit808;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Trips extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.trip_planner);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_trips, menu);
         return true;
     }
 
@@ -37,15 +34,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void switchToArrival(View view) {
-        Intent intent = new Intent(this, BusStopSearchActivity.class);
-        startActivity(intent);
-    }
-
-    public void switchToTrips(View view) {
-        Intent intent = new Intent(this, Trips.class);
-        startActivity(intent);
     }
 }
