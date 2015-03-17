a682f6559f313b990e6d9aa417eafb3f3e40e942
Eduard Gamiao <edgamiao@gmail.com>
2015-03-16 16:29:52 -1000

    Basic arrival time function working.

--- app/src/main/java/ics466uhm/transit808/StopDetails.java
+++ app/src/main/java/ics466uhm/transit808/StopDetails.java
@@ -1,5 +1,6 @@
 package ics466uhm.transit808;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
@@ -7,6 +8,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import org.apache.http.HttpEntity;
@@ -15,23 +19,37 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 
-public class StopDetails extends ActionBarActivity {
+public class StopDetails extends ListActivity {
+    private ListAdapter adapter;
+    private ListView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_details);
         Intent intent = getIntent();
-        TextView title = (TextView) findViewById(R.id.details_stop_title);
-        String stopID = intent.getStringExtra(BusStopSearchActivity.BUS_STOP_ID);
-        title.setText(intent.getStringExtra(BusStopSearchActivity.STREET_NAME_MESSAGE + "(#") + stopID + ")");
+        TextView title = (TextView) findViewById(R.id.stop_title);
+        title.append(intent.getStringExtra(BusStopSearchActivity.STREET_NAME_MESSAGE));
         RetrieveFeed feed = new RetrieveFeed();
-        feed.execute(prepareURL(stopID));
+        feed.execute(prepareURL(intent.getStringExtra(BusStopSearchActivity.BUS_STOP_ID)));
     }
 
     public String prepareURL(String busStopID) {
@@ -63,16 +81,36 @@ public class StopDetails extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Async task to read XML from URL.
+     * Code from www.androidhive.info/2011/11/android-xml-parsing-tutorial
+     */
     class RetrieveFeed extends AsyncTask<String, Integer, String> {
+        static final String PARENT_ELEMENT = "arrival";
+        static final String KEY_ID = "id";
+        static final String KEY_ROUTE = "route";
+        static final String KEY_HEADSIGN = "headsign";
+        static final String KEY_STOPTIME = "stopTime";
+        private ArrayList<HashMap<String, String>> arrivals = new ArrayList<HashMap<String, String>>();
+
         public RetrieveFeed() {
         }
 
         @Override
         protected String doInBackground(String... params) {
-            if (params != null || params.length > 0) {
-                Log.i("PARAM CHECK", params.toString());
-                getXMLFromURL(params[0]);
+            String xml = getXMLFromURL(params[0]);
+            Document doc = this.getDomElement(xml);
+            NodeList nodeList = doc.getElementsByTagName(PARENT_ELEMENT);
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                HashMap<String, String> map = new HashMap<String, String>();
+                Element e = (Element) nodeList.item(i);
+                map.put(KEY_ROUTE, getValue(e, KEY_ROUTE));
+                map.put(KEY_HEADSIGN, getValue(e, KEY_HEADSIGN));
+                map.put(KEY_STOPTIME, getValue(e, KEY_STOPTIME));
+                arrivals.add(map);
             }
+
             return "SUCCESS";
         }
 
@@ -91,8 +129,55 @@ public class StopDetails extends ActionBarActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Log.i("XML", xml);
             return xml;
+        }
+
+        public Document getDomElement(String xml) {
+            Document doc = null;
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+            try {
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                InputSource is = new InputSource();
+                is.setCharacterStream(new StringReader(xml));
+                doc = db.parse(is);
+
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return doc;
+        }
+
+        public String getValue(Element item, String str) {
+            NodeList nodeList = item.getElementsByTagName(str);
+            return this.getElementValue(nodeList.item(0));
+        }
+
+        public final String getElementValue(Node element) {
+            Node child;
+            if (element != null) {
+                if (element.hasChildNodes()) {
+                    for (child = element.getFirstChild(); child != null; child = child.getNextSibling()) {
+                        if(child.getNodeType() == Node.TEXT_NODE) {
+                            return child.getNodeValue();
+                        }
+                    }
+                }
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            adapter = new SimpleAdapter(StopDetails.this, arrivals, R.layout.stop_item,
+                    new String[] {KEY_ROUTE, KEY_HEADSIGN, KEY_STOPTIME},
+                    new int[] {R.id.route, R.id.headsign, R.id.arrivalTime});
+            setListAdapter(adapter);
         }
     }
 }
